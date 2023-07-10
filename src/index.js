5f74f7273ef17c990f394eed180b73aa4408cc5c
NotYourAbu <68179601+SyntaxErrorSolos@users.noreply.github.com>
2023-07-10 22:41:28 +0500

    basic stuff
    uwu

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { Guilds, GuildMessages, GuildMessageReactions, MessageContent } =
+  GatewayIntentBits;
+const { User, GuildMember, ThreadMember, Reaction, Message } = Partials;
+const client = new Client({
+  intents: [Guilds, GuildMessages, GuildMessageReactions, MessageContent],
+  partials: [User, GuildMember, ThreadMember, Reaction, Message],
+  allowedMentions: { parse: ["users"] },
+});
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+require("dotenv");
+
+client.login(process.env.DISCORD_TOKEN);
